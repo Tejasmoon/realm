33ee9f99cdf272cb80fb8cf29afbb5fe9235bb44
Tejasmoon <54758931+Tejasmoon@users.noreply.github.com>
2025-10-05 18:05:31 +0530

    Add files via upload

---
+++ _app/immutable/nodes/2.D2c0-ydF.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/DaL6L7dT.js";import{f as a,a as p}from"../chunks/XSS0Ar2w.js";var t=a("<h1>Hello Firend</h1>");function i(o){var r=t();p(o,r)}export{i as component};
