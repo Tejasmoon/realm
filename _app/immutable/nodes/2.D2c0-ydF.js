dd2d125de8cb9d58ac0f633b0e3cf8c48517bc29
Tejasmoon <54758931+Tejasmoon@users.noreply.github.com>
2025-10-05 18:29:43 +0530

    updated

--- _app/immutable/nodes/2.D2c0-ydF.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/DsnmJJEf.js";import"../chunks/DaL6L7dT.js";import{f as a,a as p}from"../chunks/XSS0Ar2w.js";var t=a("<h1>Hello Firend</h1>");function i(o){var r=t();p(o,r)}export{i as component};
